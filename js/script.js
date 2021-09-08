e22c96cbcc35d9bfc20981158938a0f2526dd87f
Ilyas Seisov <ilyasseisov@gmail.com>
2021-09-08 06:32:04 -0700

    remove script

--- js/script.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function () {
-  const cards = document.querySelectorAll(".cards__single-card");
-  cards[0].classList.remove("slide-in-left");
-  cards[1].classList.remove("scale-in-center");
-  cards[2].classList.remove("slide-in-right");
-}, 3000);
